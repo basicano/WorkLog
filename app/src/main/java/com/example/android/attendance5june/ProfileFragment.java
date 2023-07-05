8b1b70eb8478be3f7266bdc322994b189d5fa07b
Ritika Mohanty <67157042+basicano@users.noreply.github.com>
2023-07-05 22:03:45 +0530

    Update ProfileFragment.java

--- app/src/main/java/com/example/android/attendance5june/ProfileFragment.java
+++ app/src/main/java/com/example/android/attendance5june/ProfileFragment.java
@@ -41,10 +41,13 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+
+// declares a Java class named ProfileFragment which extends the Fragment class. It also implements the VolleyJsonResponseListener interface.
 public class ProfileFragment extends Fragment implements VolleyJsonResponseListener{
     public static final String TAG = ProfileFragment.class.getSimpleName();
     private static String in_out_status = "in";
 
+    // declare several variables of different types (TextView, Button, LinearLayout, SharedPreferences, and String) that will be used in the class.
     TextView tv_name, tv_email, tv_intime;
     Button mark_att, change_password_btn, logout_btn, view_attendance_record_btn, manage_emp_btn, change_qrcode_btn;
     LinearLayout admin_controls;
@@ -73,6 +76,8 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
             progressDialog.dismiss();
     }
 
+    // method is overridden from the Fragment class and is responsible for creating the view hierarchy associated with the fragment. 
+    // It inflates a layout file named fragment_profile, initializes the view elements, and returns the inflated view.
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -82,24 +87,37 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
         return view;
     }
 
+
+    // method initializes the view elements and sets up a progress dialog with a message. 
     private void initView(View view) {
         progressDialog = new ProgressDialog(getContext());
         progressDialog.setCancelable(false);
         progressDialog.setMessage("Please Wait");
+
+        // find the TextView elements with the IDs tv_name and tv_email from the provided view. 
         tv_name = (TextView) view.findViewById(R.id.tv_name);
         tv_email = (TextView) view.findViewById(R.id.tv_email);
+
+        // They also retrieve shared preferences using the pref_name string resource and load the preferences into the PrefsUserInfo singleton instance. 
         pref = getActivity().getSharedPreferences(getString(R.string.pref_name), 0);
         PrefsUserInfo.getInstance().loadPreferences(getActivity());
+        // The name and email values from the preferences are then set as text for the respective TextView elements.
         tv_name.setText(PrefsUserInfo.getInstance().name);
         tv_email.setText(PrefsUserInfo.getInstance().email);
         tv_intime = (TextView) view.findViewById(R.id.tv_intime);
-        admin_controls = (LinearLayout) view.findViewById(R.id.admin_controls);
-                
+
+        // the tv_intime TextView and admin_controls LinearLayout are found using their respective IDs. 
+        // If the user is an admin (based on the value of is_admin in the preferences), the visibility of the admin_controls layout is set to View.VISIBLE.
+        admin_controls = (LinearLayout) view.findViewById(R.id.admin_controls);        
         if(PrefsUserInfo.getInstance().is_admin){
             admin_controls.setVisibility(View.VISIBLE);
         }
 
+        
         try {
+            // calculates the difference in days between the current date and the stored attendance date. 
+            // It retrieves the current date and the stored attendance date from the PrefsUserInfo singleton instance. 
+            // The dates are parsed using the SimpleDateFormat class. 
             String str_date1 = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
             String str_date2 = PrefsUserInfo.getInstance().att_date;
             Log.e(TAG,"date current -- "+ str_date1);
@@ -109,11 +127,14 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
             SimpleDateFormat dates = new SimpleDateFormat("yyyy-MM-dd");
             date1 = dates.parse(str_date1);                                     // when this date is parsed the time is 00:00
             date2 = dates.parse(str_date2);
+
+            // The difference in milliseconds is calculated, and then converted to the difference in days. 
             long difference = Math.abs(date1.getTime() - date2.getTime());
             long differenceDates = difference / (24 * 60 * 60 * 1000);          // this indicates the number of days between these two dates
             String dayDifference = Long.toString(differenceDates);
             Log.e(TAG, "Diff - "+dayDifference);
 
+            // Depending on the difference, the tv_intime TextView is updated with either "-- : --" or "In: [in_time]". The in_out_status variable is also set accordingly.
             if(differenceDates > 0) {
                 tv_intime.setText("-- : --");
                 in_out_status = "in";
@@ -125,6 +146,7 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
             //  Toast.makeText(getActivity(), "Unable to find difference", Toast.LENGTH_SHORT).show();
         }
 
+        // the button is found using its ID. A click listener is set for the button, and when clicked, it calls the listener method.
         mark_att = (Button) view.findViewById(R.id.mark_attendance);
         mark_att.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -133,6 +155,7 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
             }
         });
 
+        
         logout_btn = (Button) view.findViewById(R.id.btn_logout);
         logout_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -210,12 +233,17 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
         });
     }
 
-    public final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 112;
+    
+    public final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 112; // value of 112 is used to identify a specific permission request.
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public boolean checkPermission() {
         int currentAPIVersion = Build.VERSION.SDK_INT;
+        
+        // Checks if the current API version is higher than Android Lollipop (5.0).
         if (currentAPIVersion > Build.VERSION_CODES.LOLLIPOP) {
             if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                // Checks if the app should show a rationale for requesting the permission.
                 if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION)) {
                     AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getActivity());
                     alertBuilder.setCancelable(true);
@@ -243,12 +271,17 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
         }
     }
 
+    // handle the user's response to a permission request
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // checking the requestCode parameter to determine which permission request it is handling. This is useful when an activity requests multiple permissions and needs to differentiate between them.
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // if the permission was granted  length of grantResults should be greater than 0, indicating that at least one permission result is available. 
+                    // the specific permission result is obtained from the grantResults array.
                     gps = new GPSTracker(getActivity());
+                    // If the permission is granted, the code executes the corresponding actions. In the provided code, it creates a GPSTracker object (presumably for accessing GPS location) and displays a toast message indicating that GPS is enabled.
                     Toast.makeText(getActivity(), "GPS Enabled, Go for Scanning ...", Toast.LENGTH_SHORT).show();
                 } else {
                     //  code for deny
@@ -257,16 +290,19 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
         }
     }
 
-
-
+    // for displaying the attendance record of an employee. 
     private void view_attendance_record() {
+        // creates an instance of the EmpViewAttFragment fragment and replaces the current fragment in the fragment_frame container
         Fragment fragment = new EmpViewAttFragment();
         FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction().setReorderingAllowed(true);
         ft.replace(R.id.fragment_frame,fragment);
         ft.commit();
     }
 
+    // handles the functionality of changing the user's password
     private void change_password() {
+
+        // shows an alert dialog with input fields for the current password, new password, and password verification.
         final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View view = inflater.inflate(R.layout.dailog_change_password, null);
@@ -284,6 +320,7 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
         view.findViewById(R.id.btnChangePassword).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //  validates the input and sends a request to the server to change the password.
                 String current_password = currentPassword.getText().toString().trim();
                 String new_password = newPassword.getText().toString().trim();
                 String verify_password = verifyPassword.getText().toString().trim();
@@ -331,7 +368,9 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
         });
     }
 
+    // method handles the logout functionality
     private void logout() {
+        // the shared preferences to mark the user as logged out and replaces the current fragment with the LoginFragment, effectively redirecting the user to the login screen.
         SharedPreferences.Editor editor = pref.edit();
         editor.putBoolean(getString(R.string.is_logged_in), false);
         editor.apply();
@@ -341,6 +380,7 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
         ft.commit();
     }
 
+    // method is responsible for marking attendance. It uses the GPSTracker class to obtain the user's current GPS location. If the location is available and the user has granted the necessary permissions, it checks the distance between the user's location and the office location. If the user is within a certain distance, it starts the QRCodeScanner activity to scan a QR code for attendance marking.
     private void mark_attendance() {
         gps = new GPSTracker(getActivity());
         gps.getLocation();
@@ -385,9 +425,11 @@ public class ProfileFragment extends Fragment implements VolleyJsonResponseListe
         }
     }
 
+    //  invoked when a JSON request is successful
     @Override
     public void onSuccessJson(String response, String type) {
         Log.e(TAG, "reached onSuccessJson() ");
+        // checks if the type parameter is equal to a specific value (type_cng_pass). If they are equal, it means that the JSON request was for changing the password.
         if(type.equalsIgnoreCase(type_cng_pass)){
             try {
                 JSONObject jsonObject = new JSONObject( response );
