2170dbb8c112618ab02a20165df0da8c98c34823
Ritika Mohanty <67157042+basicano@users.noreply.github.com>
2023-07-06 01:29:20 +0530

    Update BarcodeDetectorHolder.java

--- app/src/main/java/com/example/android/attendance5june/QRCode/BarcodeDetectorHolder.java
+++ app/src/main/java/com/example/android/attendance5june/QRCode/BarcodeDetectorHolder.java
@@ -15,8 +15,14 @@ class BarcodeDetectorHolder {
      *     the context
      * @return the barcode detector
      */
+
+    // to ensure that only one instance of BarcodeDetector is created and reused throughout the application. 
+    // By using the static getBarcodeDetector method, other parts of the code can easily obtain the BarcodeDetector instance without having to manage its creation and initialization.
     static BarcodeDetector getBarcodeDetector(Context context) {
+        // Context object is typically used to access resources and information about the application's environment.
         if (detector == null) {
+            // new instance of BarcodeDetector is created using the BarcodeDetector.Builder class. 
+            // The builder is configured with the application context obtained from the parameter, and the barcode format is set to Barcode.QR_CODE.
             detector = new BarcodeDetector.Builder(context.getApplicationContext()).setBarcodeFormats(
                     Barcode.QR_CODE).build();
         }
